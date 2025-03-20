0dd8fcb807c976600423a54a42f9230b8f6626be
Riperto Adriel Aaron <32024230+himuraxkenji@users.noreply.github.com>
2025-03-19 23:10:24 -0300

    refactor - change function declaration

--- chapter-01/script-refactored.js
+++ chapter-01/script-refactored.js
@@ -6,11 +6,11 @@ function statement(invoice, plays) {
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf)        
         // Agregar línea de detalle a la factura
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
 
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
@@ -50,7 +50,7 @@ function volumeCreditsFor(aPerformance){
     return result
 }
 
-function format(aNumber) {
+function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
         {
             style: "currency", currency: "USD",
