f465f192d94b5befa0d97e41a5aa1430732d1ba9
Riperto Adriel Aaron <32024230+himuraxkenji@users.noreply.github.com>
2025-03-21 02:32:34 -0300

    refactor - move function

--- chapter-01/script-refactored.js
+++ chapter-01/script-refactored.js
@@ -11,6 +11,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(aPerformance)
     result.amount = amountFor()
+    result.volumeCredits = volumeCreditsFor(aPerformance)
     return result;
   }
 
@@ -39,6 +40,14 @@ function statement(invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
 }
 
 function renderPlainText(data, plays) {
@@ -61,22 +70,17 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
+
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
       {
         style: "currency", currency: "USD",
         minimumFractionDigits: 2
       }).format(aNumber / 100);
-  }
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 
 }
