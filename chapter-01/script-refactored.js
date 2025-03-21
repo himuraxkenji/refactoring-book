58f79343d6d43a99afdccd3bbd6e86ff91c374f7
Riperto Adriel Aaron <32024230+himuraxkenji@users.noreply.github.com>
2025-03-21 02:17:35 -0300

    refactor - move object

--- chapter-01/script-refactored.js
+++ chapter-01/script-refactored.js
@@ -1,10 +1,12 @@
 function statement (invoice, plays) {
-    const statementData = {}
+    const statementData = {
+      customer: invoice.customer
+    }
     return renderPlainText(statementData, invoice, plays) 
 }
 
 function renderPlainText(data, invoice, plays){
-    let result = `Statement for ${invoice.customer}\n`;
+    let result = `Statement for ${data.customer}\n`;
     for (let perf of invoice.performances) {
       result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
