b017b722033c7c318ab3e9350345da97cdda4445
Riperto Adriel Aaron <32024230+himuraxkenji@users.noreply.github.com>
2025-03-21 00:33:03 -0300

    refactor - rename variable

--- chapter-01/script-refactored.js
+++ chapter-01/script-refactored.js
@@ -57,9 +57,9 @@ function usd(aNumber) {
 }
 
 function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf)
+        result += volumeCreditsFor(perf)
     }
-    return volumeCredits
+    return result
 }
