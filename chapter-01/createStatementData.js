d714ad76a692fda63577717d0a2091dacca62f75
Riperto Adriel Aaron <32024230+himuraxkenji@users.noreply.github.com>
2025-03-23 20:10:03 -0300

    refactor - replace condition with polimorfism

--- chapter-01/createStatementData.js
+++ chapter-01/createStatementData.js
@@ -16,21 +16,7 @@ class PerformanceCalculator {
 
 
     get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy":
-               throw 'bad things';
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
-        }
-        return result;
+        throw new Error('subclass responsability')
     }
 
     get volumeCredits() {
@@ -41,7 +27,7 @@ class PerformanceCalculator {
     }
 }
 
-class TragedyCalculator extends PerformanceCalculator{
+class TragedyCalculator extends PerformanceCalculator {
     get amount() {
         let result = 40000;
         if (this.performance.audience > 30) {
@@ -51,7 +37,7 @@ class TragedyCalculator extends PerformanceCalculator{
     }
 }
 
-class ComedyCalculator extends PerformanceCalculator{
+class ComedyCalculator extends PerformanceCalculator {
     get amount() {
         let result = 30000;
         if (this.performance.audience > 20) {
