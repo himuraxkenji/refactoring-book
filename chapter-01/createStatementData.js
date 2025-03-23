e8079e27baf2858a311a01f53a6dcd0814c1d7e5
Riperto Adriel Aaron <32024230+himuraxkenji@users.noreply.github.com>
2025-03-23 20:06:10 -0300

    refactor - Replace Conditional with Polymorphism

--- chapter-01/createStatementData.js
+++ chapter-01/createStatementData.js
@@ -14,15 +14,12 @@ class PerformanceCalculator {
         this.play = aPlay;
     }
 
+
     get amount() {
         let result = 0;
         switch (this.play.type) {
             case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
+               throw 'bad things';
             case "comedy":
                 result = 30000;
                 if (this.performance.audience > 20) {
@@ -44,8 +41,34 @@ class PerformanceCalculator {
     }
 }
 
+class TragedyCalculator extends PerformanceCalculator{
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator{
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result
+    }
+}
+
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay)
+    switch (aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`unknown type: ${aPlay.type}`);
+    }
 }
 
 function enrichPerformance(aPerformance) {
